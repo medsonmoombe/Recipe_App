b3eb5673a7c2dc6bfb24dc6c9f557ebfc47bdc07
medsonmoombe <medsonmoombe21@gmail.com>
2022-09-07 12:31:55 +0200

    Add the validations and relationships in the recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, class_name: 'User'
+  has_many :recipe_food, class_name: "RecipeFood", dependent: :destroy
+  has_many :food, through: :recipe_food
+
+  validates :name, presence: true
+  validates :public, inclusion: { in: [true, false] }
 end

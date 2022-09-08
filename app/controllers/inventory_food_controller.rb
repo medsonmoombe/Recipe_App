b9a2bc739b2b46dcacf09a165b16d4d91afe7f32
unknown <ibr.s.ahmat@gmail.com>
2022-09-08 17:20:24 +0300

    Fix linter errors

--- app/controllers/inventory_food_controller.rb
+++ app/controllers/inventory_food_controller.rb
@@ -1,8 +1,8 @@
 class InventoryFoodController < ApplicationController
   def create
     @food = Food.includes(:user).find(params[:food_id])
-    @inventoryFood = InventoryFood.create(inventory_params)
-    @inventoryFood.user = current_user
+    @inventory_food = InventoryFood.create(inventory_params)
+    @inventory_food.user = current_user
     if @inventoryFood.save
       flash[:success] = 'New inventory food has been created !!'
       redirect_to inventory_path(params[:id])
@@ -13,8 +13,8 @@ class InventoryFoodController < ApplicationController
   end
 
   def destroy
-    @inventoryFood = InventoryFood.find(params[:id])
-    @inventoryFood.destroy
+    @inventory_food = InventoryFood.find(params[:id])
+    @inventory_food.destroy
     flash[:success] = 'Inventory has been deleted successfully'
     redirect_to inventories_path
   end

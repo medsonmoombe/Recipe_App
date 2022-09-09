905a637f9bb86b88b5b557efaba034f9f5940f55
unknown <ibr.s.ahmat@gmail.com>
2022-09-09 11:05:44 +0300

    Edits on the relationship (foods, inventory)

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,6 +2,7 @@ class Food < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
 
   has_many :inventory_foods, dependent: :delete_all
+  has_many :inventories, through: :inventory_foods, dependent: :destory
 
   validates :measurement_unit, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,7 +2,7 @@ class Inventory < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
 
   has_many :inventory_foods, dependent: :delete_all
-  has_many :foods, through: :inventory_foods, dependent: :delete_all
+  has_many :foods, through: :inventory_foods, dependent: :destory
 
   validates :name, presence: true
 end

2bee419613071f088ea3459845ca3bf7ae642b77
medsonmoombe <medsonmoombe21@gmail.com>
2022-09-10 00:53:20 +0200

    Add the public foods controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :user
   root 'recipe#index'
+  match 'public_recipes' => 'public_recipes#index', as: :public_recipes, via: :get
 
   resources :users, only: [:index, :show] do
     resources :recipe, only: [:index, :new, :show, :create, :destroy]
@@ -10,6 +11,10 @@ Rails.application.routes.draw do
   resources :foods, only: [:index, :show, :new, :create, :destroy]
 
   patch '/recipe/:id', to: 'recipe#toggle_public', as: :update_recipe
+  get '/shopping_list/:recipe_id', to: 'recipe_food#shoppinglist', as: :shopping_list
+  
+
+
 
   resources :recipe, only: [:index, :show, :destroy] do
     resources :recipe_food, only: [:create, :new, :edit, :update, :destroy]

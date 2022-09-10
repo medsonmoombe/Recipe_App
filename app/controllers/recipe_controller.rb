24cd9ab37dbbd905461a51176254ff82f413994d
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-09-10 15:40:24 +0100

    add change

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,5 +1,5 @@
 class RecipeController < ApplicationController
-  load_and_authorize_resource
+  # load_and_authorize_resource
 
   def index
     @recipes = current_user.recipes.includes(:user, :recipe_foods).order(created_at: :desc)

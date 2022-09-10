fc062af09c405af435b8b5d5dc7511064bea74ce
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-09-10 12:49:55 +0100

    Remove rubocop error

--- spec/models/foods_spec.rb
+++ spec/models/foods_spec.rb
@@ -2,31 +2,31 @@ require 'rails_helper'
 
 RSpec.describe Food, type: :model do
   before(:each) do
-    @food = Food.new(name: "Apple", measurement_unit: "g", price: 1.5)
+    @food = Food.new(name: 'Apple', measurement_unit: 'g', price: 1.5)
   end
 
-  it "should have a name" do
+  it 'should have a name' do
     @food.name = nil
     expect(@food).to_not be_valid
   end
 
-  it "should have a measurement unit" do
+  it 'should have a measurement unit' do
     @food.measurement_unit = nil
     expect(@food).to_not be_valid
   end
 
-  it "should have a price" do
+  it 'should have a price' do
     @food.price = nil
     expect(@food).to_not be_valid
   end
 
-  it "should associate with a logged in user" do
+  it 'should associate with a logged in user' do
     @food.user_id = nil
     expect(@food).to_not be_valid
   end
 
-  it "should have a name that is at least 3 characters long" do
-    @food.name = "Ap"
+  it 'should have a name that is at least 3 characters long' do
+    @food.name = 'Ap'
     expect(@food).to_not be_valid
   end
 end
